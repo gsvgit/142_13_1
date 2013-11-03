92bc4d22ace8fb054b617e2e53cf50345c16a67b
Lares77 <lares77@yandex.ru>
2013-11-03 16:08:43 +0400

    Задача 14

---
+++ gsv/KovalevDmitry/Задача 14/Program.fs
@@ -0,0 +1,16 @@
+﻿let arr1 = [|-1; 45; 1; 0; -97; 123; 31|]
+let arr2 = [|-1000; 0; 0; 0; -890; -3|]
+
+let main (inArr: int array) =
+    let k = ref 0
+    for i in 0..inArr.Length - 2 do
+        for j in 0..inArr.Length - 2 do
+        if inArr.[j] > inArr.[j + 1] 
+        then
+            k := inArr.[j]
+            inArr.[j] <- inArr.[j + 1]
+            inArr.[j + 1] <- !k
+    inArr
+
+printfn "%A" (main arr1)
+printfn  "%A" (main arr2)
