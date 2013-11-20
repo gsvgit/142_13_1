274d1599c879c4b41b6dbea2804566443185efda
m13oas <aozertsov@gmail.com>
2013-11-20 21:44:44 +0400

    fixed

--- OzertsovAlexander/18/18/tests.fs
+++ OzertsovAlexander/18/18/tests.fs
@@ -1,8 +1,8 @@
-﻿printfn "recursive %A" (Reclist.main 10)
-printfn "iterative with ref %A" (Iterlist.main 10)
-printfn "iterative without ref %A" (Iterwref.main 10)
-printfn "matrix %A" (Matrix.main 10)
-printfn "matrix per log %A" (Matrixlog.main 10)
-printfn "array of numbers %A" (Allfib.main 10)
+﻿printfn "recursive %A" (Task.recurs 10)
+printfn "iterative with ref %A" (Task.iteration 10)
+printfn "iterative without ref %A" (Task.iterarionwithoutref 10)
+printfn "matrix %A" (Task.matrix 10)
+printfn "matrix per log %A" (Task.matrixlog 10)
+printfn "array of numbers %A" (Task.fibarray 10)
 
 
