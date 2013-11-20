19efc7383a2e719e07b60e548d050722e1ebad6f
m13oas <aozertsov@gmail.com>
2013-11-20 18:24:46 +0400

    used solutions

---
+++ OzertsovAlexander/18/18/tests.fs
@@ -0,0 +1,8 @@
+﻿printfn "recursive %A" (Reclist.main 10)
+printfn "iterative with ref %A" (Iterlist.main 10)
+printfn "iterative without ref %A" (Iterwref.main 10)
+printfn "matrix %A" (Matrix.main 10)
+printfn "matrix per log %A" (Matrixlog.main 10)
+printfn "array of numbers %A" (Allfib.main 10)
+
+
