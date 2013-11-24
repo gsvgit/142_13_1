d8eba983f89dbfd89af2b7074e3a325db63d4331
gsv <rsdpisuy@gmail.com>
2013-11-25 00:18:31 +0400

    Example of trees.

---
+++ gsv/Trees/Trees/Program.fs
@@ -0,0 +1,18 @@
+﻿type MyList =
+| Lst of int*MyList
+| Empty
+
+let lst = Lst(1, Lst(2, Empty))
+
+lst |> printfn "%A" 
+
+type Tree =
+| Node of int*Tree*Tree*Tree 
+| Leaf of int
+
+let leafs = Leaf(6), 1
+let nd = Node(2013,
+            Node(1, Leaf(1), Leaf(2), Leaf(3)), 
+            Node(1, Leaf(4), Leaf(0), Leaf(10)),
+            Leaf(3))
+nd |> printfn "%A"
