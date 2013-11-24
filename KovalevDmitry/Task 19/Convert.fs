0f3d19d4510b56a9e128d2e57027009f2a09d311
Lares77 <lares77@yandex.ru>
2013-11-24 23:20:29 +0400

    Task 19

---
+++ KovalevDmitry/Task 19/Convert.fs
@@ -0,0 +1,19 @@
+﻿let main (f: float) =
+
+    let arrByte = System.BitConverter.GetBytes(f) 
+    printfn "%A" arrByte
+    printfn ""
+    
+    let (b: byte) = 1uy;    
+    
+    let (arrBit: byte array) = Array.create 8 0uy
+    
+    for i in 0..arrByte.Length - 1 do
+        for j in 0..7 do
+            arrBit.[j] <- ((arrByte.[i] >>> j) &&& b)        
+        printfn "%A = %A" i (Array.map int arrBit)         
+    printfn ""
+
+main (65.578)
+main (-545.8787)
+main (0.0)
