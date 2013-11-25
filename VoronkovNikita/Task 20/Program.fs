7a31f82ababc154ea68c4224c075f50c0a56516c
ShBNK <nekitvoronkov@mail.ru>
2013-11-26 02:33:14 +0400

    Tasks 17 and 20

---
+++ VoronkovNikita/Task 20/Program.fs
@@ -0,0 +1,15 @@
+﻿let rec mainFilter (lst: int List) (arr: int array) = 
+    match lst with
+    |[] -> []
+    |hd::tl -> let a = ref 0
+               let i = ref 0
+               while (!a = 0 && !i < Array.length arr) do
+                   if arr.[!i] = hd
+                   then a := 5
+                   else i := !i + 1
+               if !a = 5
+               then mainFilter tl arr
+               else hd::mainFilter tl arr
+printfn "%A" (mainFilter [1; 6; 7; 7; 10] [|1; 7|])
+printfn "%A" (mainFilter [1; 1; 1; 1; 1; 1] [|1|])
+printfn "%A" (mainFilter [1; 2; 3; 4; 5; 6] [|1; 4; 9|])
