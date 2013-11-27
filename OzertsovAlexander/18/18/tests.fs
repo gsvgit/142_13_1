f8cc15ed1f02d3fd6dc19dc4fb3a0668383dda12
gsv <rsdpisuy@gmail.com>
2013-11-27 15:09:19 +0400

    Rm strange stuff.

--- OzertsovAlexander/18/18/tests.fs
+++
@@ -1,8 +0,0 @@
-﻿printfn "recursive %A" (Task.recurs 10)
-printfn "iterative with ref %A" (Task.iteration 10)
-printfn "iterative without ref %A" (Task.iterarionwithoutref 10)
-printfn "matrix %A" (Task.matrix 10)
-printfn "matrix per log %A" (Task.matrixlog 10)
-printfn "array of numbers %A" (Task.fibarray 10)
-
-
