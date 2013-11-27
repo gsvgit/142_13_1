c44bdd094fa0f8c2a3815dc670e4e5aaf5f1ad8e
Mikelle <mikhwall@gmail.com>
2013-11-27 18:06:31 +0400

    Tasks 19, 20, 21. I think that you forgot to check task 13.

---
+++ WallMikhail/Float (task 19)/Float (task 19)/Program.fs
@@ -0,0 +1,12 @@
+﻿let main (f: float) =
+
+    let byte = System.BitConverter.GetBytes(f)
+    let (b: byte) = 1uy    
+    for i in 0..byte.Length - 1 do
+        for j in 0..7 do
+            printf "%A" (int ((byte.[i] >>> j) &&& b))
+    printfn ""
+
+main (54.24)
+main (-1.27910)
+main (0.0)
---
+++ WallMikhail/mainFilter (task 20)/mainFilter (task 20)/Program.fs
@@ -0,0 +1,18 @@
+﻿let mainFilter lst (arr:int array) =
+
+    let rec del l x =
+        match l with
+        | [] -> []
+        | h :: t ->
+            if h = x
+            then  del t x
+            else  h :: (del t x)
+
+    let rec fil l i =
+        if i < arr.Length - 1
+        then
+            fil (del l arr.[i]) (i + 1)
+        else del l arr.[i]
+    fil lst 0
+
+printfn "%A" (mainFilter [1; 8; 2; 2; 3; 5] [|1;2|])    
