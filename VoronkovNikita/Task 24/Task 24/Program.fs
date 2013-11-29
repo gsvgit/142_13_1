6a3fd8c6baa1b20827a758ed0feba3065d6f5e1a
ShBNK <nekitvoronkov@mail.ru>
2013-11-29 23:19:18 +0400

    Tasks 23, 24, 27.

---
+++ VoronkovNikita/Task 24/Task 24/Program.fs
@@ -0,0 +1,12 @@
+﻿type List = 
+    | Lst of int * List
+    | Empty
+let length = ref 0
+let rec main x = 
+    match x with
+    | Lst (a, n1) -> 
+        length := !length + 1
+        main n1
+    | Empty -> !length
+let myList = Lst (1, Lst (4, Lst (9, Empty)))
+main myList |> printfn "%A"
