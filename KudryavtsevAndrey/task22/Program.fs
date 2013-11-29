791d92e4be0bf5d31efa782c2636505a602de2ad
Kudryavtsev Andrey <tmac1attack@gmail.com>
2013-11-29 14:45:00 +0400

    task22

---
+++ KudryavtsevAndrey/task22/Program.fs
@@ -0,0 +1,14 @@
+﻿
+let mainConvert (list:List<int>)  =
+    let arr = Array.zeroCreate (List.length list) 
+    let rec convert lst i = 
+        match lst with 
+        | [] -> arr
+        | hd::tl -> 
+            arr.[i] <- hd
+            convert tl (i + 1)
+    convert list 0 
+
+printfn "result = %A" (mainConvert [1; 2; 3; 4; 5])
+printfn "result = %A" (mainConvert [0])
+printfn "result = %A" (mainConvert [])
