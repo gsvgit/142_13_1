1d01f138fc429831d5de8ba8c035f69f44fc6f09
m13oas <aozertsov@gmail.com>
2013-12-01 22:49:24 +0400

    Task 27

---
+++ OzertsovAlexander/27/TreeInf/TreeInf/TreeInformation.fs
@@ -0,0 +1,22 @@
+﻿type Tree =
+    | Node of string * list<Tree>
+    | Leaf of string
+
+let main tree = 
+    let rec num tr =
+        let rec number lst =
+            match lst with
+            | hd :: tl -> num hd + number tl
+            | [] -> 0        
+        match tr with
+        | Node (s, tre) ->
+            if s.[0] = 'a'
+            then 1 + number tre
+            else number tre
+        | Leaf k ->
+            if k.[0] = 'a'
+            then 1
+            else 0 
+    num tree
+
+printfn "%A" (main (Node ("aba", [Node ("ar", [Node ("rew", [Leaf "arr"]) ]); Node ("ar", [Node ("rew", [Leaf "arr"]) ]) ])) )
