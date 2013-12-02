12cd0779bbae07434d1b218eae09c2e993e21866
ShBNK <nekitvoronkov@mail.ru>
2013-12-02 14:01:24 +0400

    Corrected 24 & 25

--- VoronkovNikita/Task 24/Task 24/Program.fs
+++ VoronkovNikita/Task 24/Task 24/Program.fs
@@ -1,15 +1,12 @@
 ﻿type List = 
     | Lst of int * List
     | Empty
-let main lst = 
-    let length = ref 0
-    let rec help x = 
-        match x with
-        | Lst (a, n1) -> 
-            length := !length + 1
-            help n1
-        | Empty -> !length
-    help lst
+let length = ref 0
+let rec main x = 
+    match x with
+    | Lst (a, n1) -> 
+        length := !length + 1
+        main n1
+    | Empty -> !length
 let myList = Lst (1, Lst (4, Lst (9, Empty)))
 main myList |> printfn "%A"
-main Empty |> printfn "%A"
