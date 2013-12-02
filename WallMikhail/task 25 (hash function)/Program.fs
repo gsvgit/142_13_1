c80ec12c829848efce49eeda6e646f17a4c65888
Mikelle <mikhwall@gmail.com>
2013-12-02 13:44:22 +0400

    fixed

--- WallMikhail/task 25 (hash function)/Program.fs
+++ WallMikhail/task 25 (hash function)/Program.fs
@@ -1,4 +1,9 @@
-﻿let stringHash s = 
-    hash s
+﻿let stringHash (s: string) = 
+    let h = ref 0
+    for i in 0..s.Length - 1 do
+        
+        if s.[i] = '1' || s.[i] = '2' || s.[i] = '3' || s.[i] = '4' || s.[i] = '5' || s.[i] = '6' || s.[i] = '7' || s.[i] = '8' || s.[i] = '9' || s.[i] = '0' 
+        then h := !h + 1
+    !h
 printfn "%A" (stringHash "sfg")
-printfn "%A" (stringHash "")
+printfn "%A" (stringHash "1fewhh3124")
