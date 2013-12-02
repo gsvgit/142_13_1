47d835c368660c10d4961ea61b9bce1603fb5767
Andrey0103 <pivolit@gmail.com>
2013-12-02 21:38:08 +0400

    23 and 24

---
+++ BulgakovAndrey/BulgakovAndrey_23/BulgakovAndrey_23/TypeList.fs
@@ -0,0 +1,6 @@
+﻿module TypeList
+type MyList =
+    | Lst of int * MyList
+    | Empty
+let k = Lst(1, Lst(2, Empty))
+printfn "%A" k
---
+++ BulgakovAndrey/BulgakovAndrey_24/BulgakovAndrey_24/Program.fs
@@ -0,0 +1,11 @@
+﻿open TypeList
+let length (lst:MyList) =
+    let k = ref 0;
+    let rec leng (list:MyList) l =
+        match list with
+        | Lst(int, MyList) -> leng MyList (l + 1) 
+        | Empty -> l
+    leng lst !k     
+        
+let k = Lst(1, Lst(2, Empty))
+printfn "%A" (length k)
