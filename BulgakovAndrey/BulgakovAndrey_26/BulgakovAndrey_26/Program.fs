8f23fcecb662c964c6c2d82e6a3a745ac1f66c35
Andrey0103 <pivolit@gmail.com>
2013-12-03 22:48:23 +0400

    New 26

---
+++ BulgakovAndrey/BulgakovAndrey_26/BulgakovAndrey_26/Program.fs
@@ -0,0 +1,9 @@
+﻿let hashAll (lst:List<string>) =
+    let main (str:string) =
+        int (str.[0])
+    let rec hash lst1 lst2=
+        match lst1 with
+        | [] -> lst2
+        | h::d -> hash d (main h::lst2)
+    List.rev (hash lst [])
+printfn "%A" (hashAll ["ld";"fdf";"fdf";"fd"])
