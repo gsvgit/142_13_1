bf28a5f66e8cbfa50d0d6ae033929bd4fac65f30
ShBNK <nekitvoronkov@mail.ru>
2013-12-04 22:13:52 +0400

    Task 28.

---
+++ VoronkovNikita/Task 28/Task 28/ListofTree.fs
@@ -0,0 +1,9 @@
+﻿open TreeFunction
+type List = 
+    | Lst of Tree * List
+    | Empty
+let rec main x = 
+    match x with
+    | [] -> []
+    | h::t -> f h::main t
+printfn "%A" (main ([Node ("1", [Node ("lalalala", [Leaf "AAA"; Leaf "tratatat"; Leaf "TravinSoft"])]); Leaf "12345678"]))
---
+++ VoronkovNikita/Task 28/Task 28/Program.fs
@@ -0,0 +1,22 @@
+﻿module TreeFunction
+
+type Tree = 
+    | Node of string * list<Tree>
+    | Leaf of string
+let f x = 
+    let count = ref 0
+    let rec main t = 
+        match t with
+        | Node (s, list) -> 
+            if String.length s = 8
+            then 
+                count := !count + 1
+                for i in 0..List.length list - 1 do main list.[i]
+            else for i in 0..List.length list - 1 do main list.[i]
+        | Leaf s -> 
+            if String.length s = 8
+            then count := !count + 1 
+            else count := !count  
+    main x
+    !count
+printfn "%A" (f (Node ("12345679", [Node ("8", [Node ("sdqwerty", [Leaf ("331 cows"); Leaf ("33 cows"); Leaf ("Wendigoo")]); Leaf ("1")]); Leaf ("HelloWor")])))
