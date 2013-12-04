468c5e2fd4370ab5468705090a37789c757d9db0
Kudryavtsev Andrey <tmac1attack@gmail.com>
2013-12-04 22:20:20 +0400

    fix

--- KudryavtsevAndrey/task24/task24/Program.fs
+++ KudryavtsevAndrey/task24/task24/Program.fs
@@ -1,11 +1,14 @@
-﻿open myType
+﻿//open myType
 
-type myList =
-| Lst of int*myList
-| Empty
 
-let rec lstLength (lst, myList) =
+type myList = 
+    | Lst of int * myList
+    | Empty
+
+let rec lstLength lst = 
     match lst with 
-    | lst (int, MyList) -> lstLength myList + 1
-    | Empty -> 0 
+    | Lst (int, myList) -> lstLength myList + 1
+    | Empty -> 0
 
+printfn "%A" (lstLength (Lst (124, Lst (24, Empty))))
+printfn "%A" (lstLength (Empty))
