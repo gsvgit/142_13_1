f425e9e9d8f2d5d919d899fed2e2e7f6975a7331
ShBNK <nekitvoronkov@mail.ru>
2013-12-04 15:57:17 +0400

    Corrected 25 and new 26

---
+++ VoronkovNikita/Task 26/Task 26/hashAll.fs
@@ -0,0 +1,7 @@
+﻿open hash
+let rec hashAll x = 
+    match x with
+    | [] -> []
+    | h::t -> stringHash h::hashAll t
+printfn "%A" (hashAll ["London is"; " the"; " capital of G"; "reat Britain"])
+printfn "%A" (hashAll ["Kansas"; "Dust in"; " "; "the       wind"])
