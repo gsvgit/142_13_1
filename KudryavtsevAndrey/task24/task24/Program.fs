ef234d1c70f94a4d8a5385d15583c41e4dc7ef4d
Kudryavtsev Andrey <tmac1attack@gmail.com>
2013-12-04 22:08:08 +0400

    new 21, 23

---
+++ KudryavtsevAndrey/task24/task24/Program.fs
@@ -0,0 +1,11 @@
+﻿open myType
+
+type myList =
+| Lst of int*myList
+| Empty
+
+let rec lstLength (lst, myList) =
+    match lst with 
+    | lst (int, MyList) -> lstLength myList + 1
+    | Empty -> 0 
+
