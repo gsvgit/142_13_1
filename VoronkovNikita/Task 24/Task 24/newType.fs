0fdfed01ce6ed495251c1865ed2a246300ecbc9c
ShBNK <nekitvoronkov@mail.ru>
2013-12-04 15:09:44 +0400

    New programs 24 & 25

--- VoronkovNikita/Task 24/Task 24/newType.fs
+++ VoronkovNikita/Task 24/Task 24/newType.fs
@@ -1,4 +1,6 @@
-﻿type List = 
+﻿module myType
+
+type List = 
     | Lst of int * List
     | Empty
 let myList = Lst (1, Lst (4, Lst (9, Lst (16, Lst (25, Empty)))))
--- VoronkovNikita/Task 25/Task 25/Program.fs
+++ VoronkovNikita/Task 25/Task 25/Program.fs
@@ -1,12 +1,19 @@
 ﻿let stringHash (str: string) = 
     let a = ref 0
-    if str.[str.Length - 2] <> ' ' 
-    then a := !a + 1
-    for i in 1..str.Length - 1 do
-        if str.[i] = ' ' && str.[i - 1] <> ' '
+    if str.Length = 1 && str.[0] = ' ' || str.Length  = 0
+    then a := 0
+    elif (str.[0] <> ' ' && (str.Length = 1 || str.Length = 2)) || (str.[0] <> ' ' && str.[1] = ' ' && str.Length = 2)
+    then a := 1
+    else
+        if str.[str.Length - 2] = ' ' && str.[str.Length - 1] <> ' '
         then a := !a + 1
-    !a
+        for i in 1..str.Length - 1 do
+            if str.[i] = ' ' && str.[i - 1] <> ' '
+            then a := !a + 1
+    if str.[str.Length - 2] <> ' ' && str.[str.Length - 1] <> ' '
+    then !a + 1
+    else !a
 stringHash "              AC\DC                       " |> printfn "%A"
 stringHash "      Back in black             " |> printfn "%A"
 stringHash "  Rock'n'roll train" |> printfn "%A"
-stringHash "Highway to hell" |> printfn "%A"
+stringHash "1 2 3" |> printfn "%A"
