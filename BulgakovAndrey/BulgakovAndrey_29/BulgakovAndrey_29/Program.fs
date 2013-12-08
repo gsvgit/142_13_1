fd42706586e2b7d839057d48e01355a6c6932093
Andrey0103 <pivolit@gmail.com>
2013-12-08 22:51:13 +0400

    All programs.

---
+++ BulgakovAndrey/BulgakovAndrey_29/BulgakovAndrey_29/Program.fs
@@ -0,0 +1,17 @@
+﻿open TreeListTree
+type MyList =
+    | Lst of Tree * MyList
+    | Empty
+let SomethingHard tre =
+    let rec f tr =
+        match tr with
+        | h :: d -> TreeListTree.inf h :: f d
+        | [] -> []
+    f tre
+printfn "%A" <|
+(SomethingHard 
+[
+Node ("bl", [Node ("bb", [Node ("bl", [Leaf "block1"])]); Node ("bl", [Node ("bb", [Node ("bl", [Leaf "block2"]) ]) ]) ]);
+Node ("bl", [Node ("bb", [Node ("bl", [Leaf "block1"])]); Node ("bl", [Node ("bb", [Node ("bl", [Leaf "block2"]) ]) ]) ])
+]
+)
