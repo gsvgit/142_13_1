1668e9ba121c87bac788a88e05c293d15b71cbec
Kudryavtsev Andrey <tmac1attack@gmail.com>
2013-12-10 02:05:38 +0400

    new 28

---
+++ KudryavtsevAndrey/task28 (list of tree with function)/task28/Program.fs
@@ -0,0 +1,12 @@
+﻿open TreeFunction
+
+type listOfTree =
+    | Lst of Tree * listOfTree
+    | Empty
+
+let rec main (list: listOfTree) = 
+    match list with 
+    | Lst (tree, lst) -> treeInfo tree :: main lst
+    | Empty -> []
+
+main (Lst (Node ("1", [Node ("2", [Leaf "3"; Leaf "4"]) ]), Empty)) |> printfn "%A"
