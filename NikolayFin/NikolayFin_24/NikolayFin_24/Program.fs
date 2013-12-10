65e198eddc81d35b35c0f602b2971df9738c7e77
NikolayFin <finashenkov.brazas@yadnex.ru>
2013-12-10 19:29:32 +0400

    I corrected, please check.

--- NikolayFin/NikolayFin_24/NikolayFin_24/Program.fs
+++ NikolayFin/NikolayFin_24/NikolayFin_24/Program.fs
@@ -1,10 +1,10 @@
 ﻿open typeList
 
-let rec main (list:MyList) i =
+let rec main (list:MyList) =
     match list with 
-    | Lst (int, myList) -> main myList i + 1
-    | Empty -> i
+    | Lst (int, myList) -> main myList + 1
+    | Empty -> 0
 
 let myList1 = Lst (1, Lst (2, Lst (3, Lst (4, Lst (5, Empty)))))
 
-printfn "myList1.Length = %A" (main myList1 0)
+printfn "myList1.Length = %A" (main myList1)
--- NikolayFin/NikolayFin_28/NikolayFin_28/Program.fs
+++ NikolayFin/NikolayFin_28/NikolayFin_28/Program.fs
@@ -10,7 +10,7 @@ let rec main (list:listOfTree) =
     | Empty -> []
 
 let myList = Lst (Node ("abc", [Node ("def", [Leaf "hij"; Leaf "klm"; Leaf "nop"]);Node ("qrs", [Leaf "tuv"; Leaf "wm"; Leaf "yz"])]),
-Lst (Node ("bc", [Node ("def", [Leaf "klm"; Leaf "nop"]);Node ("qrs", [Leaf "wm"; Leaf "yz"])]),
-Lst (Node ("bc", [Node ("df", [Leaf "klm"; Leaf "np"]);Node ("qrs", [Leaf "tu"; Leaf "wm"; Leaf "y"])]), Empty)))
+                Lst (Node ("bc", [Node ("def", [Leaf "klm"; Leaf "nop"]);Node ("qrs", [Leaf "wm"; Leaf "yz"])]),
+                    Lst (Node ("bc", [Node ("df", [Leaf "klm"; Leaf "np"]);Node ("qrs", [Leaf "tu"; Leaf "wm"; Leaf "y"])]), Empty)))
 
 printfn "%A" (main myList)
