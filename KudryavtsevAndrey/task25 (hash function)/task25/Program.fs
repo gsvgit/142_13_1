a060888d975404daee7a79b5de4f38903c6d46c2
Kudryavtsev Andrey <tmac1attack@gmail.com>
2013-12-10 16:37:08 +0400

    fix 25

--- KudryavtsevAndrey/task25 (hash function)/task25/Program.fs
+++ KudryavtsevAndrey/task25 (hash function)/task25/Program.fs
@@ -4,8 +4,12 @@ let hashString (str: string) =
     let a = ref 0
     for i in 0..str.Length - 1 do 
         a := !a + int(str.[i])
-    a := !a / str.Length 
-    !a 
+    if !a = 0
+    then 0
+    else
+        a := !a / str.Length 
+        !a 
 printfn "%A" (hashString "a")
 printfn "%A" (hashString "ab")
 printfn "%A" (hashString "az")
+printfn "%A" (hashString "")
