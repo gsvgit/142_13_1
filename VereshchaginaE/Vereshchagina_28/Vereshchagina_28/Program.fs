d83fc40ecb527f6015bf5d5113020a6ee8581277
VereshchaginaE <kryska18@mail.ru>
2013-12-11 22:02:20 +0400

    new 27 and 28

---
+++ VereshchaginaE/Vereshchagina_28/Vereshchagina_28/Program.fs
@@ -0,0 +1,17 @@
+﻿open Tree
+
+type ListOfTree =
+    | Lst of Tree.Tree * ListOfTree
+    | Empty
+
+let rec func (list:ListOfTree) = 
+    match list with 
+    | Lst (tree, lst) -> treeHash tree :: func lst
+    | Empty -> []
+
+
+printfn "%A" (func (Lst (Node ("ghbdtn", [Node ("pkjq", [Leaf "rjdfhysq"]) ]), Empty)))
+printfn "%A" (func (Empty))
+
+
+
