3fee237f8d409e5fa26f93169c806e24db07446b
GovorukhaOksana <gos_1996@mail.ru>
2013-12-11 20:55:45 +0400

    new 23, 24, 25, 26

---
+++ GovorukhaOksana/GovorukhaOksana_23/GovorukhaOksana_23/GovorukhaOksana_23.fs
@@ -0,0 +1,9 @@
+﻿module public typeList
+
+type myList = 
+| Lst of int * myList
+| Empty
+
+let x = Lst (0, Lst (1, Lst (11, Lst (111, Empty))))
+
+x |> printfn "%A"
---
+++ GovorukhaOksana/GovorukhaOksana_24/GovorukhaOksana_24/Program.fs
@@ -0,0 +1,16 @@
+﻿open typeList
+
+let lengthList( x: myList) = 
+    let l = ref 0
+    let rec count ( x: myList) = 
+        match x with
+        | Lst(int, myList) -> l := !l + 1
+                              count myList
+        | Empty -> !l
+    count x
+    
+let lt =  Lst (0, Lst (1, Lst (11, Lst (111, Empty))))
+let lt1 = Empty
+
+lengthList lt |> printfn "%A"
+lengthList lt1|> printfn "%A"
