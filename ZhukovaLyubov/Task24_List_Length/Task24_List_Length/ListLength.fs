62e8cba4344daf3f4d531ec0eb9acb47616ebe16
ZhukovaLyubov <jeanevesse.yeah@gmail.com>
2013-12-11 10:49:44 +0400

    New

---
+++ ZhukovaLyubov/Task24_List_Length/Task24_List_Length/ListLength.fs
@@ -0,0 +1,8 @@
+﻿open ListType
+
+let rec listLength list =
+    match list with
+        | Lst (int, List) -> listLength List + 1
+        | Empty -> 0
+printfn "%A" (listLength (Lst(5, Lst(13, Lst(22, Empty)))))
+printfn "%A" (listLength (Empty))
