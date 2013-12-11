9f4e87095653d848d91f443a6111badf90def699
Andrey0103 <pivolit@gmail.com>
2013-12-11 22:56:45 +0400

    Fixed 26

--- BulgakovAndrey/BulgakovAndrey_26/BulgakovAndrey_26/Program.fs
+++ BulgakovAndrey/BulgakovAndrey_26/BulgakovAndrey_26/Program.fs
@@ -2,6 +2,6 @@
     let rec hash lst1 lst2=
         match lst1 with
         | [] -> lst2
-        | h::d -> hash d (Hashfun.main h::lst2)
+        | h :: d -> hash d (Hashfun.main h :: lst2)
     List.rev (hash lst [])
-printfn "%A" (hashAll ["ld";"fdf";"fdf";"fd"])
+printfn "%A" (hashAll ["ld"; "fdf"; "fdf"; "fd"])
