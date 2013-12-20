912d724d4068ee6c0f38c0b96190b44356d5a219
Kudryavtsev Andrey <tmac1attack@gmail.com>
2013-12-20 23:57:22 +0400

    fix 19

--- KudryavtsevAndrey/task19/task_19/Program.fs
+++ KudryavtsevAndrey/task19/task_19/Program.fs
@@ -4,8 +4,7 @@ let main (f: float) =
     printfn "%A" f
 
     let arrByte = System.BitConverter.GetBytes(f)      
-    let (b: byte) = 1uy    
-    let (arrBit: byte array) = Array.create 8 0uy
+    let b = 1uy    
     for i in 0..arrByte.Length - 1 do
         for j in 0..7 do
             printf "%A" (int ((arrByte.[i] >>> j) &&& b))                      
