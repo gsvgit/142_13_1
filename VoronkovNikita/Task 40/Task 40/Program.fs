08a4eb61a52befb33810a75070b63adbf7703289
ShBNK <nekitvoronkov@mail.ru>
2013-12-21 02:16:54 +0400

    Tasks 37 & 40.

---
+++ VoronkovNikita/Task 40/Task 40/Program.fs
@@ -0,0 +1,12 @@
+﻿let compress a b c d =
+    let byteArr = [|a; b; c; d|]
+    System.BitConverter.ToInt32 (byteArr, 0)
+let s = compress 12uy 34uy 56uy 234uy
+printfn "%A" s
+
+let extract (x: int32) = 
+    System.BitConverter.GetBytes x
+let k = extract 13
+printfn "%A" k
+
+printfn "%A" (extract s)
