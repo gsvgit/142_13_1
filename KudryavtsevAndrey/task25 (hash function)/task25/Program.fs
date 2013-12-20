27b8ed79fa886673136772f6d4c352d2c52def4d
Kudryavtsev Andrey <tmac1attack@gmail.com>
2013-12-21 00:17:23 +0400

    fix 25 (spaces)

--- KudryavtsevAndrey/task25 (hash function)/task25/Program.fs
+++ KudryavtsevAndrey/task25 (hash function)/task25/Program.fs
@@ -3,7 +3,7 @@
 let hashString (str: string) = 
     let a = ref 0
     for i in 0..str.Length - 1 do 
-        a := !a + int(str.[i])
+        a := !a + int str.[i]
     if !a = 0
     then 0
     else
