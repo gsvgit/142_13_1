b4cee4a872acc40ac3a9dc8603f3c93ffdb2d3b2
Andrey0103 <pivolit@gmail.com>
2013-12-21 21:01:53 +0400

    Vereshchagina, new 19-22

---
+++ VereshchaginaE/Vereshchagina_22/Backup/Vereshchagina_22/Program.fs
@@ -0,0 +1,13 @@
+﻿let mainConvert (lst : List<int>) =
+    let arr = Array.zeroCreate lst.Length 
+    let rec convert (lst1 : List<int>) (arr1 : array<int>) i = 
+        match lst1 with
+        | hd::tl ->             
+            arr.[i] <- hd
+            convert tl arr1 (i + 1)            
+        | [] -> arr
+    convert lst arr 0
+
+printfn "%A" (mainConvert [1; 3; 1; 2; 1; 1; 9])
+printfn "%A" (mainConvert [])
+
