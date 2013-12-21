8581a09ac71605b586338478b384d78cf13d605b
VereshchaginaE <kryska18@mail.ru>
2013-12-21 14:36:11 +0400

    New 19, 20, 21, 22

---
+++ VereshchaginaE/Vereshchagina_19/Vereshchagina_19/Program.fs
@@ -0,0 +1,15 @@
+﻿let main (fl : float) =
+    let arr = System.BitConverter.GetBytes fl   
+    for i in arr do       
+        for j in 0..7 do
+            if (i >>> j) % 2uy = 1uy
+            then
+                printf "1"
+            else 
+                printf "0"
+               
+
+printfn "0.34756587"
+(main 0.34756587)
+
+
---
+++ VereshchaginaE/Vereshchagina_20/Vereshchagina_20/Program.fs
@@ -0,0 +1,25 @@
+﻿let mainFilter (lst1 : List<int>) (arr1 : array<int>) =
+    let rec search i x =
+            if  (x <> arr1.[i]) && (i < arr1.Length) 
+            then
+                if i = arr1.Length - 1
+                then false
+                else search (i + 1) x
+            else true
+    
+    let rec filter list = 
+        if arr1 = [||]
+        then list
+        else
+            match list with
+            | hd::tl ->  
+                if search 0 hd
+                    then filter tl 
+                    else hd::filter tl 
+            | [] -> []     
+    
+    filter lst1
+
+printfn "%A" (mainFilter [] [|0; 1; 2; 3; 4; 5; 6; 7; 8; 9|])
+printfn "%A" (mainFilter [1; 2; 3; 4; 5; 6; 7; 8; 9] [||])
+printfn "%A" (mainFilter [1; 3; 1; 2; 1; 1; 9] [|0; 2|])
