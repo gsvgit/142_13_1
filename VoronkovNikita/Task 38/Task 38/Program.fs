9c112a7ada779f8da11cd4a6bb40e630a09c80cb
ShBNK <nekitvoronkov@mail.ru>
2013-12-21 21:42:19 +0400

    Corrected tasks 16, 18, 21 & 37. New tasks 28 & 39.

---
+++ VoronkovNikita/Task 38/Task 38/Program.fs
@@ -0,0 +1,109 @@
+﻿module Multip
+
+open myType
+
+let main (lst1: List) (lst2: List) = 
+    
+    let rec reverse (lst: List) =
+        let rec returnLast lst =
+            match lst with
+            | Lst (h, Empty) -> h
+            | Lst (h, t) -> returnLast t
+            | Empty -> 0
+        let rec cut lst =
+            match lst with 
+            | Lst (h, Empty) -> Empty
+            | Lst (h, t) -> Lst (h, cut t)
+            | Empty -> Empty
+        match lst with
+        | Empty -> Empty
+        | Lst (h, t) -> Lst (returnLast lst, reverse (cut lst))
+    
+    let findingTail (x: List) = 
+        match x with
+        | Lst (h, t) -> t
+        | Empty -> Empty 
+    
+    let findingHead (x: List) = 
+        match x with
+        | Lst (h, t) -> h
+        | Empty -> 0
+
+    let rec checkDigit lt = 
+        match lt with
+        | Lst (h, t) -> if h < 0 || h > 10
+                        then 10
+                        else checkDigit t
+        | Empty -> 0
+
+    if lst1 = Empty || 
+       lst2 = Empty ||
+       findingTail lst2 = Empty ||
+       findingTail lst1 = Empty
+    then failwith "Error! You didn't enter at least one number."
+
+    elif findingHead lst1 <> 1 && findingHead lst1 <> -1 || 
+       findingHead lst2 <> 1 && findingHead lst2 <> -1
+    then failwith "Error! You entered wrong sign (first element in every list). Sign must be equal 1 or -1"
+    
+    elif checkDigit (findingTail lst1) = 10 ||
+         checkDigit (findingTail lst2) = 10
+    then failwith "Error! You entered wrong digit. Digit must be from 0 to 9."
+
+    else
+        let lst1Rev = reverse (findingTail lst1)
+        let lst2Rev = reverse (findingTail lst2)
+        let rec summing lst1 lst2 s =          
+            match lst1, lst2 with   
+            | Empty, Empty -> Empty
+            | Lst (h1, t1), Empty ->
+                let hd = h1 + s
+                if s = 1 
+                then Lst (hd, t1)            
+                else Lst (h1, t1)
+            | Empty, Lst (h2, t2) ->    
+                let hd = h2 + s
+                if s = 1 
+                then Lst (hd, t2)            
+                else Lst (h2, t2) 
+            | Lst (h1, t1), Lst (0, Empty) -> Lst (h1, t1)
+            | Lst (0, Empty), Lst (h2, t2) -> Lst (h2, t2)    
+            | Lst (h1, Empty), Lst (h2, Empty) -> 
+                let hd = h1 + h2 + s 
+                if hd >= 10 
+                then Lst (hd - 10, Lst (1, Empty))            
+                else Lst (hd, Empty)                 
+            | Lst (h1, t1), Lst (h2, t2) -> 
+                let hd = h1 + h2 + s  
+                if hd >= 10  
+                then Lst (hd - 10, summing t1 t2 1)            
+                else Lst (hd, summing t1 t2 0)
+       
+        let rec additionZeroes lst n =
+            if n > 0
+            then Lst (0, additionZeroes lst (n - 1))
+            else lst 
+                      
+        let rec help lst hd m =
+            match lst with
+            | Empty -> 
+                if m > 0
+                then Lst (m, Empty)
+                else Empty
+            | Lst (h, t) -> 
+                let hds = h * hd + m
+                if hds > 9
+                then Lst (hds % 10, help t hd (hds / 10)) 
+                else Lst (hds, help t hd 0)
+
+        let rec listsMult lst1 lst2 n =       
+            match lst2 with        
+            | Empty -> Empty        
+            | Lst (h, t) ->  summing (additionZeroes (help lst1 h 0) n) (listsMult lst1 t (n + 1)) 0 
+                                                                        
+        Lst (findingHead lst1 * findingHead lst2, reverse (listsMult lst1Rev lst2Rev 0))
+
+main (Lst (1, Lst (2, Lst (3, Empty)))) (Lst (1, Lst (2, Lst (3, Empty)))) |> printfn "%A"
+main (Lst (1, Lst (9, Empty))) (Lst (-1, Lst (2, Lst (3, Lst (3, Empty))))) |> printfn "%A"
+main (Lst (1, Lst (2, Empty))) Empty |> printfn "%A"
+
---
+++ VoronkovNikita/Task 39/Task 39/Program.fs
@@ -0,0 +1,39 @@
+﻿open myType
+open Add
+open Multip
+
+let findingTail lst = 
+    match lst with
+    | Lst (h, t) -> t
+    | Empty -> Empty
+
+type Long =
+    | Num of List
+    static member (++) ((l1:Long), (l2:Long)) =
+        match l1,l2 with
+        | Num lst1, Num lst2 -> Num (Add.main (Lst (1, lst1)) (Lst (1, lst2)) |> findingTail)  
+    static member (+*) ((l1:Long), (l2:Long)) =
+        match l1,l2 with
+        | Num lst1, Num lst2 -> Num (Multip.main (Lst (1, lst1)) (Lst (1, lst2)) |> findingTail) 
+
+let main n = 
+    let fibN = [|Num (Lst (1, Empty)); Num (Lst (1, Empty)); Num (Lst (1, Empty)); Num (Lst (0, Empty))|]
+    let fibN1 = [|Num (Lst (1, Empty)); Num (Lst (1, Empty)); Num (Lst (1, Empty)); Num (Lst (0, Empty))|]
+    let fibN2 = [|Num (Lst (1, Empty)); Num (Lst (1, Empty)); Num (Lst (1, Empty)); Num (Lst (0, Empty))|]
+  
+    if n > 0 
+    then
+        for i in 1..n - 1 do
+            fibN.[0] <- (fibN1.[0] +* fibN2.[0]) ++ (fibN1.[1] +* fibN2.[2])
+            fibN.[1] <- (fibN1.[0] +* fibN2.[1]) ++ (fibN1.[1] +* fibN2.[3])
+            fibN.[2] <- (fibN1.[2] +* fibN2.[0]) ++ (fibN1.[3] +* fibN2.[2])
+            fibN.[3] <- (fibN1.[2] +* fibN2.[1]) ++ (fibN1.[3] +* fibN2.[3])
+            fibN1.[0] <- fibN.[0]
+            fibN1.[1] <- fibN.[1]
+            fibN1.[2] <- fibN.[2]
+            fibN1.[3] <- fibN.[3]
+        fibN.[2]
+    else 
+        failwith "Error! Wrong index!"
+
+main 6 |> printfn "%A"
