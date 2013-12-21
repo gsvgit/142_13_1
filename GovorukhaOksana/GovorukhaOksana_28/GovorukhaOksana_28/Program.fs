3eb140a593b6de5f86edd6ac121ecf3b35ab40d5
GovorukhaOksana <gos_1996@mail.ru>
2013-12-21 20:27:00 +0400

    fix 27, 28, 46

--- GovorukhaOksana/GovorukhaOksana_28/GovorukhaOksana_28/Program.fs
+++ GovorukhaOksana/GovorukhaOksana_28/GovorukhaOksana_28/Program.fs
@@ -7,12 +7,15 @@ type ListOfTree =
 let main (lstree: ListOfTree) =
     let rec f lstree =
         match lstree with
-        | Lst( tree, lsttr) -> Trees.maxLeaves tree :: f lsttr
+        | Lst (tree, lsttr) -> Trees.maxLeaves tree :: f lsttr
         | Empty -> []
 
     f lstree
 
-main (Lst (Node ("gdg", [Leaf(""); Leaf(""); Leaf(""); Node ("dd", [Leaf ("")])]), 
-           Lst (Node ("gdg", [ Leaf(""); Node ("dd", [Leaf (""); Leaf("")])]), Empty))) |> printfn "%A"
+main (Lst (Node ("gdg", [Leaf (""); Leaf (""); Leaf (""); Node ("dd", [Leaf ("")])]), 
+           Lst (Node ("gdg", [Leaf(""); Node ("dd", [Leaf (""); Leaf ("")])]), Empty))) |> printfn "%A"
 
+main (Lst (Leaf (""), Empty)) |> printfn "%A"
+main (Lst (Node ("h", [Leaf ("")]), Empty)) |> printfn "%A"
+main (Lst (Leaf (""), Lst (Leaf(""), Empty))) |> printfn "%A"
     
--- GovorukhaOksana/GovorukhaOksana_46/GovorukhaOksana_46/Program.fs
+++ GovorukhaOksana/GovorukhaOksana_46/GovorukhaOksana_46/Program.fs
@@ -2,29 +2,28 @@
 open Trees
 open typeList
 
-let main (tr: Tree) = 
+let main tr = 
     let n = ref 0
     let lst1 = ref Empty
-    let f2 (lst: myList) (i: int) = 
+    let f2 lst i = 
         match lst with
-        | Lst (int, myList) -> Lst (i, Lst (int,myList))  
+        | Lst (int, myList) -> Lst (i, Lst (int, myList))  
         | Empty -> Lst (i, Empty) 
     
     
-    let rec f (lst: myList) (tr: Tree) =
-        
+    let rec f lst tr =
         match tr with
-        | Node (str, list) ->  lst1 := f2 lst (Hash.stringHash(str)) 
-                               for i in 0 .. (List.length list - 1 ) do 
-                                   f (f2 !lst1 (Hash.stringHash(str))) list.[i] 
-                                   
+        | Node (str, list) ->  
+               lst1 := f2 lst (Hash.stringHash(str)) 
+               for i in 0 .. (List.length list - 1) do 
+                   f (f2 !lst1 (Hash.stringHash(str))) list.[i] 
         | Leaf (str) -> lst1 := f2 !lst1 (Hash.stringHash(str)) 
     
     f Empty tr
 
-    lst1
+    !lst1
 
-main (Node("au", [Leaf "rdooge "; Leaf "aoieughy"])) |> printfn "%A"
+main (Node("au", [Leaf ("rdooge "); Leaf ("aoieughy")])) |> printfn "%A"
     
 
     
