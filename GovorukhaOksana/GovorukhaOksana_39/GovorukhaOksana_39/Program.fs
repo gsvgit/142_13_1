57b202f748bffd48e06db8a3d886a63b61ddcaec
GovorukhaOksana <gos_1996@mail.ru>
2013-12-23 00:46:50 +0400

    fix 39

--- GovorukhaOksana/GovorukhaOksana_39/GovorukhaOksana_39/Program.fs
+++ GovorukhaOksana/GovorukhaOksana_39/GovorukhaOksana_39/Program.fs
@@ -17,7 +17,7 @@ type Long =
         | Num list, Num list2 -> Num (Multiplication.main (Lst (1, list)) (Lst (1, list2)) |> t)     
 
 let matr x =
-    let n = [|Num (Lst (1, Empty)); Num (Lst (1, Empty)); Num (Lst (1, Empty)); Num (Lst (0, Empty))|]
+    let n = [|Num (Lst (0, Empty)); Num (Lst (0, Empty)); Num (Lst (0, Empty)); Num (Lst (0, Empty))|]
     let n2 = [|Num (Lst (1, Empty)); Num (Lst (1, Empty)); Num (Lst (1, Empty)); Num (Lst (0, Empty))|]
     let n3 = [|Num (Lst (1, Empty)); Num (Lst (1, Empty)); Num (Lst (1, Empty)); Num (Lst (0, Empty))|]
     if x <= 0 
@@ -33,7 +33,7 @@ let matr x =
             n2.[1] <- n.[1]
             n2.[2] <- n.[2]
             n2.[3] <- n.[3]
-        n.[2] 
+        n.[1] 
         
 
 matr 6 |> printfn "%A" 
