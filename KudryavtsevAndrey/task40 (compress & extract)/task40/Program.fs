34093bdb6291091382f2aaf61350573d85ef161d
Kudryavtsev Andrey <tmac1attack@gmail.com>
2013-12-22 21:09:14 +0400

    fix 40

--- KudryavtsevAndrey/task40 (compress & extract)/task40/Program.fs
+++ KudryavtsevAndrey/task40 (compress & extract)/task40/Program.fs
@@ -4,18 +4,25 @@
     arrByte.[1] <- byte ((x &&& 0x0000ff00) >>> 8)
     arrByte.[2] <- byte ((x &&& 0x00ff0000) >>> 16)
     arrByte.[3] <- byte ((x &&& 0xff000000) >>> 24)
-    arrByte
-
+    arrByte.[0], arrByte.[1], arrByte.[2], arrByte.[3] 
+   
 printfn "%A" (extract 2281448)
 printfn "%A" (System.BitConverter.GetBytes 2281448)
 
 
-let compress (byteArr: array<byte>) = 
+let compress (byte1, byte2, byte3, byte4) = 
+    let byteArr = Array.create 4 0uy
+    byteArr.[0] <- byte1
+    byteArr.[1] <- byte2
+    byteArr.[2] <- byte3
+    byteArr.[3] <- byte4
     let intArr = byteArr |> Array.map int    
     let y = 0 + int intArr.[0]  + (intArr.[1] <<< 8) + (intArr.[2] <<< 16) + (intArr.[3] <<< 24)
     y
     
-printfn "%A" (compress [|187uy; 228uy; 14uy; 88uy|])  
+printfn "%A" (compress (187uy, 228uy, 14uy, 88uy))
 printfn "%A" (System.BitConverter.ToInt32 ([|187uy; 228uy; 14uy; 88uy|], 0))
 
-printfn "%A" (compress (extract 1234567890))
+extract 123 |> compress |> printfn "%A" 
+
+
