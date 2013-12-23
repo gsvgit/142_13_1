07eff6b89c74fea69a5c8d4045eb16ec0795fedd
Andrey0103 <pivolit@gmail.com>
2013-12-23 14:00:04 +0400

    test2

---
+++ BulgakovAndrey/BulgakovAndrey_14/BulgakovAndrey_14/Program.fs
@@ -0,0 +1,13 @@
+﻿module Bubble
+let main (arr : int[]) =      
+    for i in 0..arr.Length - 1 do
+        for j in 0..arr.Length - 2 - i do
+            if arr.[j] > arr.[j + 1]
+            then
+                arr.[j] <- arr.[j] + arr.[j + 1]
+                arr.[j + 1] <- arr.[j] - arr.[j + 1]
+                arr.[j] <- arr.[j] - arr.[j + 1]
+    arr
+printfn "res = %A" (main [|8; 7; 9; 1; 1|])
+printfn "res = %A" (main [||])    
+            
